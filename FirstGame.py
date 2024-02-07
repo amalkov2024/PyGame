6ed0296adb84c6056bb99283ef8aaea9ba5f422d
malkovaa2024 <157360537+malkovaa2024@users.noreply.github.com>
2024-02-07 13:37:23 +0500

    v3 Hello PyGame square green

--- FirstGame.py
+++ FirstGame.py
@@ -14,6 +14,17 @@ def draw(screen):
     pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
                                            text_w + 20, text_h + 20), 1)
 
+def draw_square(screen):
+    color = pygame.Color(50, 150, 50)
+    # рисуем "тень"
+    pygame.draw.rect(screen, color,
+                     (20, 20, 100, 100), 0)
+    hsv = color.hsva
+    # увеличиваем параметр Value, который влияет на яркость
+    color.hsva = (hsv[0], hsv[1], hsv[2] + 30, hsv[3])
+    # рисуем сам объект
+    pygame.draw.rect(screen, color, (10, 10, 100, 100), 0)
+
 if __name__ == '__main__':
     # инициализация Pygame:
     pygame.init()
@@ -25,6 +36,7 @@ if __name__ == '__main__':
     # команды рисования на холсте
 
     draw(screen)
+    draw_square(screen)
 
     # смена (отрисовка) кадра:
     pygame.display.flip()
