0720cf41150c216c5a9b9641e3990d1d80f41356
malkovaa2024 <157360537+malkovaa2024@users.noreply.github.com>
2024-02-08 12:45:59 +0500

    v4

--- k_shelchku.py
+++ k_shelchku.py
@@ -1,8 +1,8 @@
 import pygame
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pygame.init()
-    pygame.display.set_caption('К щелчку')
+    pygame.display.set_caption("К щелчку")
     size = width, height = 501, 501
     screen = pygame.display.set_mode(size)
     x_pos = 250
@@ -21,20 +21,20 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                x_y=event.pos # координаты нажатия мышки
+                x_y = event.pos  # координаты нажатия мышки
                 # print(x_y)
-                dx = (x_y[0] - x_pos)
-                dy = (x_y[1] - y_pos)
+                dx = x_y[0] - x_pos
+                dy = x_y[1] - y_pos
                 flag = True
 
-        while (abs(x_pos) != abs(x_y[0]) or abs(y_pos)!=abs(x_y[1])) and flag:
+        while (abs(x_pos) != abs(x_y[0]) or abs(y_pos) != abs(x_y[1])) and flag:
             screen.fill((0, 0, 0))
-            x_pos += -1*(dx<0)+(dx>0)
-            y_pos += -1*(dy<0)+(dy>0)
+            x_pos += -1 * (dx < 0) + (dx > 0)
+            y_pos += -1 * (dy < 0) + (dy > 0)
             # print(x_pos, y_pos)
             screen.fill((0, 0, 0))
             pygame.draw.circle(screen, (255, 0, 0), (int(x_pos), int(y_pos)), 20)
-            if x_pos==x_y[0] or y_pos==x_y[1]:
+            if x_pos == x_y[0] or y_pos == x_y[1]:
                 break
             clock.tick(fps)
             pygame.display.flip()
