36363247a6e28b4047076e88b85df4f471af3eef
malkovaa2024 <157360537+malkovaa2024@users.noreply.github.com>
2024-02-27 08:23:08 +0500

    V1 от 27-02-2024 бомбочки

--- Boom them all.py
+++ Boom them all.py
@@ -3,74 +3,71 @@ import sys
 import random
 import pygame
 
-'''
-class Bomb(pygame.sprite.Sprite):
-    image = load_image("data/bomb.png")
-    image_boom = load_image("data/boom.png")
 
-    def __init__(self, group):
+# Изображение не получится загрузить
+# без предварительной инициализации pygame
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join("data", name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Bomb(pygame.sprite.Sprite):
+    image = load_image("bomb.png")
+    image_boom = load_image("boom.png")
+
+    def __init__(self, *group):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
-        # Это очень важно !!!
-        super().__init__(group)
+        # Это очень важно!!!
+        super().__init__(*group)
         self.image = Bomb.image
         self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(width)
-        self.rect.y = random.randrange(height)
+        self.rect.x = random.randrange(width - self.rect[2] - 20) + self.rect[2] // 2
+        self.rect.y = random.randrange(height - self.rect[3] - 20) + self.rect[3] // 2
 
     def update(self, *args):
-        self.rect = self.rect.move(random.randrange(3) - 1,
-                                   random.randrange(3) - 1)
-        if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
-                self.rect.collidepoint(args[0].pos):
+        if (
+            args
+            and args[0].type == pygame.MOUSEBUTTONDOWN
+            and self.rect.collidepoint(args[0].pos)
+        ):
+            self.image_boom = pygame.transform.scale(
+                self.image_boom, (self.rect[2] + 5, self.rect[3] + 5)
+            )
             self.image = self.image_boom
-'''
+
 
 # создадим группу, содержащую все спрайты
 all_sprites = pygame.sprite.Group()
-
-# создадим спрайт
-sprite = pygame.sprite.Sprite()
-# определим его вид
-sprite.image = pygame.image.load("data/bomb.png")
-
-# и размеры
-sprite.rect = sprite.image.get_rect()
-print(sprite.rect)
-# добавим спрайт в группу
-all_sprites.add(sprite)
-sprite.rect.x = 5
-sprite.rect.y = 20
-
-# изображение должно лежать в папке data
-bomb_image = pygame.image.load("data/bomb.png")
-
-for i in range(50):
-    # можно сразу создавать спрайты с указанием группы
-    bomb = pygame.sprite.Sprite(all_sprites)
-    bomb.image = bomb_image
-    bomb.rect = bomb.image.get_rect()
-
-    # задаём случайное местоположение бомбочке
-    bomb.rect.x = random.randrange(480-bomb.rect[2])+bomb.rect[2]//2
-    bomb.rect.y = random.randrange(480-bomb.rect[3])+bomb.rect[3]//2
+for _ in range(20):
+    Bomb(all_sprites)
 
 
 if __name__ == "__main__":
-    # инициализация Pygame:
-    #bomb = Bomb()
-    pygame.init()
-    size = width, height = 500, 500
-    screen = pygame.display.set_mode(size)
-
     running = True
-    screen.fill((0, 0, 0))
     while running:
+        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
-        # в главном игровом цикле
+                all_sprites.update(event)
         all_sprites.draw(screen)
         pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
