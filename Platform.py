4ef9f756631d2a7d7492035377c93a79a24d9696
malkovaa2024 <157360537+malkovaa2024@users.noreply.github.com>
2024-02-27 14:27:42 +0500

    V1 от 27-02-2024 бомбочки и столкновение

--- Platform.py
+++ Platform.py
@@ -1,32 +1,30 @@
 import pygame
-import random
 
-
-pygame.init()
-size = width, height = 500, 500
-screen = pygame.display.set_mode(size)
-all_sprites = pygame.sprite.Group()
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         super().__init__(all_sprites)
         self.add(vertical_borders)
-        self.image = pygame.Surface((50, 10),
-                                    pygame.SRCALPHA, 32)
-        pygame.draw.rect(self.image, pygame.Color("gray"), (0, 0, 50, 10), )
+        self.image = pygame.Surface((50, 10), pygame.SRCALPHA, 32)
+        pygame.draw.rect(
+            self.image,
+            pygame.Color("gray"),
+            (0, 0, 50, 10),
+        )
         self.rect = pygame.Rect(x, y, 50, 10)
-
 
 
 class Rect(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__(all_sprites)
-        self.x=x
-        self.y=y
-
-        self.image = pygame.Surface((20, 20),
-                                    pygame.SRCALPHA, 32)
-        pygame.draw.rect(self.image, pygame.Color("blue"), (0,0,20,20),)
+        self.x = x
+        self.y = y
+        self.image = pygame.Surface((20, 20), pygame.SRCALPHA, 32)
+        pygame.draw.rect(
+            self.image,
+            pygame.Color("blue"),
+            (0, 0, 20, 20),
+        )
         self.rect = pygame.Rect(x, y, 20, 20)
         self.vx = 0
         self.vy = 1
@@ -42,39 +40,41 @@ class Rect(pygame.sprite.Sprite):
         self.rect = self.rect.move(m, 0)
 
 
-
-
-fps = 50
+fps = 20
 clock = pygame.time.Clock()
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+all_sprites = pygame.sprite.Group()
 
 
 if __name__ == "__main__":
     vertical_borders = pygame.sprite.Group()
     running = True
-    flag=False
+    flag = False
     while running:
         screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button==1:
+                if event.button == 1:
                     # нажатие левой кнопки
-                    Block(event.pos[0],event.pos[1])
-
-                    #pass
-                if event.button==3:
+                    Block(event.pos[0], event.pos[1])
+                if event.button == 3:
                     # правая кнопка мыши
-                    #Rect(event.pos[0], event.pos[1])
                     if not flag:
-                        a0 = Rect(event.pos[0], event.pos[1])
+                        kub = Rect(event.pos[0], event.pos[1])
                         flag = not flag
                     else:
-                        a0.remove_rect(event.pos[0], event.pos[1])
-                    #pass
+                        kub.remove_rect(event.pos[0], event.pos[1])
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    kub.move_horizont(10)
+                if event.key == pygame.K_LEFT:
+                    kub.move_horizont(-10)
         all_sprites.draw(screen)
         all_sprites.update()
-
         clock.tick(fps)
         pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
