11b04c86d76c8de501062d2dd2d09d17c1fee494
malkovaa2024 <157360537+malkovaa2024@users.noreply.github.com>
2024-03-04 13:06:29 +0500

    V1 от 27-02-2024 допуровни

--- DopUrovni.py
+++ DopUrovni.py
@@ -7,8 +7,7 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
         self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
 
 class Player(pygame.sprite.Sprite):
@@ -16,17 +15,22 @@ class Player(pygame.sprite.Sprite):
         super().__init__(player_group, all_sprites)
         self.image = player_image
         self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
+            tile_width * pos_x + 15, tile_height * pos_y + 5
+        )
 
 
 # Отображение имен файлов с уровнями игры для предварительного просмотра и выбора нужного
-list_file = [file for file in os.listdir('data') if file[-4:]=='.txt' and file[:5]=='level']
+list_file = [
+    file for file in os.listdir("data") if file[-4:] == ".txt" and file[:5] == "level"
+]
 if list_file:
-    print('Имена файлов с доступными для загрузки уровнями:') # для отображения файлы с уровнями игры должны именоваться level*.txt
+    print(
+        "Имена файлов с доступными для загрузки уровнями:"
+    )  # для отображения файлы с уровнями игры должны именоваться level*.txt
     for f in list_file:
         print(f)
 
-level = input('Введите название файла в котором расположена карта уровня: ')
+level = input("Введите название файла в котором расположена карта уровня: ")
 pygame.init()
 pygame.key.set_repeat(200, 70)
 FPS = 50
@@ -44,12 +48,13 @@ all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 
+
 def load_image(name, color_key=None):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join("data", name)
     try:
         image = pygame.image.load(fullname).convert()
     except pygame.error as message:
-        print('Cannot load image:', name)
+        print("Cannot load image:", name)
         raise SystemExit(message)
 
     if color_key is not None:
@@ -60,22 +65,27 @@ def load_image(name, color_key=None):
         image = image.convert_alpha()
     return image
 
+
 def terminate():
     pygame.quit()
     sys.exit()
 
-def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
 
-    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+def start_screen():
+    intro_text = [
+        "ЗАСТАВКА",
+        "",
+        "Правила игры",
+        "Если в правилах несколько строк,",
+        "приходится выводить их построчно",
+    ]
+
+    fon = pygame.transform.scale(load_image("fon.jpg"), (width, height))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 50
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
+        string_rendered = font.render(line, 1, pygame.Color("black"))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
@@ -86,47 +96,46 @@ def start_screen():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 return  # начинаем игру
         pygame.display.flip()
         clock.tick(FPS)
+
 
 def load_level(filename):
     filename = "data/" + filename
     try:
         # читаем уровень, убирая символы перевода строки
-        with open(filename, 'r') as mapFile:
+        with open(filename, "r") as mapFile:
             level_map = [line.strip() for line in mapFile]
             # и подсчитываем максимальную длину
         max_width = max(map(len, level_map))
 
         # дополняем каждую строку пустыми клетками ('.')
-        return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+        return list(map(lambda x: x.ljust(max_width, "."), level_map))
     except FileNotFoundError:
-        print('Файл не найден:', filename)
-        print('Программа завершена')
+        print("Файл не найден:", filename)
+        print("Программа завершена")
         terminate()
+
 
 def generate_level(level):
     new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#':
-                Tile('wall', x, y)
-            elif level[y][x] == '@':
-                Tile('empty', x, y)
+            if level[y][x] == ".":
+                Tile("empty", x, y)
+            elif level[y][x] == "#":
+                Tile("wall", x, y)
+            elif level[y][x] == "@":
+                Tile("empty", x, y)
                 new_player = Player(x, y)
     # вернем игрока, а также размер поля в клетках
     return new_player, x, y
 
-tile_images = {
-    'wall': load_image('box.png'),
-    'empty': load_image('grass.png')
-}
-player_image = load_image('mar.png', -1)
+
+tile_images = {"wall": load_image("box.png"), "empty": load_image("grass.png")}
+player_image = load_image("mar.png", -1)
 
 tile_width = tile_height = 50
 
