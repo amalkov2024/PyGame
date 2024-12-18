766cab3b1eec8cff32acacc4c1a04ef85aae6c05
malkovaa2024 <157360537+malkovaa2024@users.noreply.github.com>
2024-12-18 17:04:18 +0500

    V2 от 18-12-2024

--- DopUrovni-2.py
+++ DopUrovni-2.py
@@ -19,36 +19,6 @@ class Player(pygame.sprite.Sprite):
         )
 
 
-# Отображение имен файлов с уровнями игры для предварительного просмотра и выбора нужного
-list_file = [
-    file for file in os.listdir("data") if file[-4:] == ".txt" and file[:5] == "level"
-]
-if list_file:
-    print(
-        "Имена файлов с доступными для загрузки уровнями:"
-    )  # для отображения файлы с уровнями игры должны именоваться level*.txt
-    for f in list_file:
-        print(f)
-
-level = input("Введите название файла в котором расположена карта уровня: ")
-pygame.init()
-pygame.key.set_repeat(200, 70)
-FPS = 50
-width = 550
-height = 550
-step = 50
-
-screen = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
-# основной персонаж
-player = None
-
-# группы спрайтов
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-
-
 def load_image(name, color_key=None):
     fullname = os.path.join("data", name)
     try:
@@ -93,10 +63,10 @@ def start_screen():
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event_game in pygame.event.get():
+            if event_game.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+            elif event_game.type == pygame.KEYDOWN or event_game.type == pygame.MOUSEBUTTONDOWN:
                 return  # начинаем игру
         pygame.display.flip()
         clock.tick(FPS)
@@ -112,7 +82,8 @@ def load_level(filename):
         max_width = max(map(len, level_map))
 
         # дополняем каждую строку пустыми клетками ('.')
-        return list(map(lambda x: x.ljust(max_width, "."), level_map))
+        # return list(map(lambda x: x.ljust(max_width, "."), level_map))
+        return [ [x for x in y] for y in list(map(lambda x: x.ljust(max_width, "."), level_map))]
     except FileNotFoundError:
         print("Файл не найден:", filename)
         print("Программа завершена")
@@ -134,6 +105,53 @@ def generate_level(level):
     return new_player, x, y
 
 
+# Проверяем возможность хода игрока. Если ход возможен то меняем место положение игрока на карте
+def step_player(player_game,karta, step_x=0, step_y=0):
+    coord_x = (player_game.rect[0] - 15) // 50
+    coord_y = (player_game.rect[1] - 5) // 50
+    flag = False
+    if step_x!=0 and 0 <= coord_x+step_x < len(karta[0]) and karta[coord_y][coord_x+step_x] != '#': # возможен ход по оси Х
+        karta[coord_y][coord_x + step_x] ='@'
+        karta[coord_y][coord_x] = '.'
+        flag = True
+    if step_y!=0 and 0 <= coord_y + step_y < len(karta) and karta[coord_y + step_y][coord_x] != '#':  # возможен ход по оси Y
+        karta[coord_y + step_y][coord_x] = '@'
+        karta[coord_y][coord_x] = '.'
+        flag = True
+    return (flag, karta)
+
+# Отображение имен файлов с уровнями игры для предварительного просмотра и выбора нужного
+list_file = [
+    file for file in os.listdir("data") if file[-4:] == ".txt" and file[:5] == "level"
+]
+if list_file:
+    print(
+        "Имена файлов с доступными для загрузки уровнями:"
+    )  # для отображения файлы с уровнями игры должны именоваться level*.txt
+    for f in list_file:
+        print(f)
+# Ввод уровня
+level_start = input("Введите название файла в котором расположена карта уровня: ")
+# карта уровня
+map_level = load_level(level_start)
+
+pygame.init()
+pygame.key.set_repeat(200, 70)
+FPS = 50
+width = 550
+height = 550
+step = 50
+
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+# основной персонаж
+# player = None
+
+# группы спрайтов
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
 # Запуск игры
 tile_images = {"wall": load_image("box.png"), "empty": load_image("grass.png")}
 player_image = load_image("mario.png", -1)
@@ -142,10 +160,9 @@ tile_width = tile_height = 50
 
 start_screen()
 # положение игрока и размер карты
-player, level_x, level_y = generate_level(load_level(level))
-# print(level_x, level_y)
-running = True
+player, level_x, level_y = generate_level(map_level)
 
+running = True
 while running:
 
     for event in pygame.event.get():
@@ -153,13 +170,21 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player.rect.x -= step
+                flag_step, map_level = step_player(player, map_level, -1, 0)
+                if flag_step:
+                    player.rect.x -= step
             if event.key == pygame.K_RIGHT:
-                player.rect.x += step
+                flag_step, map_level = step_player(player, map_level, 1, 0)
+                if flag_step:
+                    player.rect.x += step
             if event.key == pygame.K_UP:
-                player.rect.y -= step
+                flag_step, map_level = step_player(player, map_level, 0, -1)
+                if flag_step:
+                    player.rect.y -= step
             if event.key == pygame.K_DOWN:
-                player.rect.y += step
+                flag_step, map_level = step_player(player, map_level, 0, 1)
+                if flag_step:
+                    player.rect.y += step
 
     screen.fill(pygame.Color(0, 0, 0))
     tiles_group.draw(screen)
